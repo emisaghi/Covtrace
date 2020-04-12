01d6ebc13d6995c1a0c65eac3f2a8527bd58ecb7
Ethan Misaghi <ethan@emisaghi.com>
2020-04-12 13:30:25 -0600

    Initial Commit

---
+++ Covtrace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covtrace
+//
+//  Created by Ethan on 2020-04-12.
+//  Copyright © 2020 Covtracers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
