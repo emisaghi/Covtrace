518a0ac0563ddce208e461964bc056f041b4e02c
Ryunosuke Saito <rsaito1@jh.edu>
2020-04-15 19:20:57 +0900

    changed plist to get authorization to use gps

--- Covtrace/MapPage.swift
+++ Covtrace/MapPage.swift
@@ -2,7 +2,6 @@ import UIKit
 import MapKit
 import CoreLocation
 class MapScreen: UIViewController{
-
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var address: UILabel!
@@ -10,12 +9,18 @@ class MapScreen: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
-        // Do any additional setup after loading the view.
     }
-
-    func setUpLocationManager (){
-        locationManager.delegate = self
+    func setUpLocationManager(){
+        locationManager.delegate=self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            setUpLocationManager()
+            checkLocationAuthorization()
+        }else{
+            //
+        }
     }
     func centerView(){
         if let location = locationManager.location?.coordinate{
@@ -23,50 +28,41 @@ class MapScreen: UIViewController{
             mapView.setRegion(region, animated: true)
         }
     }
-    func checkLocationServices (){
-        if CLLocationManager.locationServicesEnabled(){
-            setUpLocationManager()
-            checkLocationAuthorization()
-            
-        }else{
-            //warning!!
-        }
-    }
     func checkLocationAuthorization(){
-        switch CLLocationManager.authorizationStatus(){
-        case .authorizedAlways:
+          switch CLLocationManager.authorizationStatus(){
+          case .authorizedAlways:
+            mapView.showsUserLocation = true
             centerView()
-            locationManager.startUpdatingLocation()
-            break
-        case .authorizedWhenInUse:
-            centerView()
-            locationManager.startUpdatingLocation()
-            //recommend to keep it on all the time and do map
-            break
-        case .denied:
-            //tell them to turn it on
-            break
-        case .notDetermined:
-            locationManager.requestAlwaysAuthorization()
-            break
-        case .restricted:
-            //show alert
-            break
-        }
+              break
+          case .authorizedWhenInUse:
+             mapView.showsUserLocation = true
+             centerView()
+              //recommend to keep it on all the time and do map
+              break
+          case .denied:
+              //tell them to turn it on
+              break
+          case .notDetermined:
+              locationManager.requestAlwaysAuthorization()
+              break
+          case .restricted:
+              //show alert
+              break
+          }
     }
 }
-extension MapScreen : CLLocationManagerDelegate{
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+extension MapScreen: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else{return}
+        print(location.coordinate.longitude)
+        print(location.coordinate.latitude)
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let region = MKCoordinateRegion.init(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
         mapView.setRegion(region, animated: true)
     }
-}
-
-extension MapScreen : MKMapViewDelegate{
-    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //
     }
 }
 
