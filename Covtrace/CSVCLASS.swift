b3fa72b9fb80aca9602cc647ea9da8863a645525
akulkarnii <60018159+akulkarnii@users.noreply.github.com>
2020-04-25 22:31:21 -0500

    Update CSVCLASS.swift

--- Covtrace/CSVCLASS.swift
+++ Covtrace/CSVCLASS.swift
@@ -8,3 +8,36 @@
 
 import Foundation
 
+//most of this is honestly just error checking, the shit in the do loop is the main reading
+func readDataFromCSV(fileName:String, fileType: String)-> String! {
+        guard let filepath = Bundle.main.path(forResource: fileName, ofType: fileType)
+        else {
+          return nil
+        }
+        do {
+            var contents = try String(contentsOfFile: filepath, encoding: .utf8)
+            contents = cleanRows(file: contents) //not sure what this does but read that this needs to be in here
+            return contents
+        } catch {
+            print("File Read Error for file \(filepath)")
+            return nil
+        }
+}
+
+func csv(data: String) -> [[String]] {
+    var result: [[String]] = []
+    let rows = data.components(separatedBy: "\n")
+    for row in rows {
+        let columns = row.components(separatedBy: ";")
+        result.append(columns)
+    }
+    return result
+}
+
+/*
+* TO PLACE IN MAIN *
+var data = readDataFromCSV(fileName: jhulinks, fileType: csv) //i think?
+data = cleanRows(file: data)
+let csvRows = csv(data: data)
+print(csvRows[1][1])
+*/
