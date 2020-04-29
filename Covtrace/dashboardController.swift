26acd9058e524c5123cecd898e10a003fbee5744
Neel Mehta <neelmehta00@gmail.com>
2020-04-28 22:43:46 -0400

    final csv

--- Covtrace/dashboardController.swift
+++ Covtrace/dashboardController.swift
@@ -35,7 +35,7 @@ class dashboardController: UIViewController{
         action:#selector(backToMap))
         
         
-        getlink()
+        
         self.county_label.text = "My County:" + COUNTY + ", " + STATE
         
         //csv caller
@@ -45,7 +45,7 @@ class dashboardController: UIViewController{
         let text = "View details about county"
         let attributedString = NSAttributedString.makeHyperlink(for: link, in: text, as: "View details about county")
         link_url.attributedText = attributedString
-        print(link)
+        
         
    }
     
@@ -57,45 +57,9 @@ class dashboardController: UIViewController{
         self.performSegue(withIdentifier: "goBackToMap", sender: self)
     }
     
-    var MD:[String:String] = ["Baltimore":"https://bao.arcgis.com/covid-19/jhu/county/24005.html"]
-    var MA:[String:String] = [
-    "Hampden":"https://bao.arcgis.com/covid-19/jhu/county/25013.html"]
-    var PA:[String:String] = [
-        "Bucks":"https://bao.arcgis.com/covid-19/jhu/county/42017.html", "Washington":"https://bao.arcgis.com/covid-19/jhu/county/42125.html"]
-    var OR:[String:String] = [
-     "Washington":"https://bao.arcgis.com/covid-19/jhu/county/41067.html"]
     
-    func getlink(){
-    /*
-        if (COUNTY == "MD"){
-        let text = "View details about county"
-        let link = self.MD[self.COUNTY] ?? ""
-        let attributedString = NSAttributedString.makeHyperlink(for: link, in: text, as: "View details about county")
-        link_url.attributedText = attributedString
-        }
-        if (STATE == "MA"){
-        let text = "View details about county"
-        let link = self.MA[self.COUNTY] ?? ""
-        let attributedString = NSAttributedString.makeHyperlink(for: link, in: text, as: "View details about county")
-        link_url.attributedText = attributedString
-        }
-        if (STATE == "PA"){
-        let text = "View details about county"
-        let link = self.PA[self.COUNTY] ?? ""
-        let attributedString = NSAttributedString.makeHyperlink(for: link, in: text, as: "View details about county")
-        link_url.attributedText = attributedString
-        }
-        if (STATE == "OR"){
-        let text = "View details about county"
-        let link = self.OR[self.COUNTY] ?? ""
-        let attributedString = NSAttributedString.makeHyperlink(for: link, in: text, as: "View details about county")
-        link_url.attributedText = attributedString
-        }
-  
-        */
-    // Do any additional setup after loading the view.
     
-    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
         if (segue.identifier == "gotoProfile") {
